117fc66b9413ef0813ce67fa2f3197309eec5a21
Error-NaN <mohitnagaraj20@gmail.com>
2023-04-14 20:47:29 +0530

    Mapped from details from api to weather card
    Little more modifications to weather card. Fetched details from api and included them in card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
-
+import { WEATHER_API_URL,WEATHER_API_KEY } from './api';
+import { useState } from 'react';
 function App() {
+  const[currentWeather,setCurrentWeather]=useState(null);
+  const[forecast,setForecast]=useState(null);
   const handleOnSearchChange=(searchData)=>{
-    console.log(searchData);
+    const [lat, lon]=searchData.value.split(" ");
+    const currentWeatherFetch=fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forecastFetch=fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    
+    Promise.all([currentWeatherFetch,forecastFetch])
+      .then(async(response)=>{
+        const weatherResponse=await response[0].json();
+        const forecastResponse=await response[1].json();
+        setCurrentWeather({city:searchData.label,...weatherResponse});
+        setForecast({city:searchData.label,...forecastResponse});
+      })
+      .catch((err)=>console.log(err));
   }
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather/>
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -6,3 +6,5 @@ export const geoApiOptions = {
 	}
 };
 export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="e81226fcc124377fe6ad70402dceb65a";
